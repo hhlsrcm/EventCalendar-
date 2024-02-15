0901debdd3532ed10a6927078c487080f08d6e54
Nikita Sannikov <Nikita.alekseevich.5@mail.ru>
2024-02-15 22:14:22 +0400

    End calendar

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const currentDate = document.querySelector('.current-date');
-
+const days = document.querySelector('.days');
+const icons = document.querySelectorAll('.arrows i');
 
 let date = new Date();
-currYear = date.getFullYear();
-currMonth = date.getMonth();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
+let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+let lastDayOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+let lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
 
 const months = ["January", "February", "March", "April", "May", "June", "July",
     "August", "September", "October", "November", "December"];
 
-(function () {
+const renderCalendar = function () {
+
+    let liDays = '';
+    for (let i = firstDayOfMonth; i > 0; i--) {
+        liDays += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
+    }
+    for (let i = 1 ; i <= lastDayOfMonth; i++){
+        liDays += `<li>${i}</li>`;
+    }
+    days.innerHTML = liDays;
     currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
-})();
+}
+icons.forEach(icon =>{
+    icon.addEventListener('click', () => {
+        currMonth = icon.className === 'bx bx-left-arrow-alt' ? currMonth - 1 : currMonth + 1;
+        renderCalendar();
+    })
+})
+renderCalendar();
---
+++ test.js
@@ -0,0 +1,4 @@
+
+
+let dayOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+console.log(dayOfMonth)
